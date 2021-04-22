d98446ec20f07c7b520ae2741f49fa78d6a2041c
Lepota Wanted <wanted.lepota@gmail>
2021-04-22 22:21:34 +0200

    rename class

--- src/main/java/za/co/lottery/service/FileProcessing.java
+++ src/main/java/za/co/lottery/service/FileProcessing.java
@@ -10,7 +10,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.time.LocalDateTime;
 
-public class ReadJSON {
+public class FileProcessing {
     private JSONObject returnJsonAsList() {
 
         JSONParser jsonParser = new JSONParser();
