147b171e3c17fff8fdd431002d152feb35226e89
Amos Sekati <amos.sekati@momentum.co.za>
2021-04-28 22:46:37 +0200

    Add persist code

--- src/main/java/za/co/lottery/controller/PowerballController.java
+++ src/main/java/za/co/lottery/controller/PowerballController.java
@@ -1,21 +1,27 @@
 package za.co.lottery.controller;
 
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import za.co.lottery.dto.PowerballDTO;
 import za.co.lottery.model.Powerball;
+import za.co.lottery.repository.PowerballRepository;
+import za.co.lottery.service.FileProcessing;
 import za.co.lottery.service.PowerballlService;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
 
+@Data
 @RestController
 @RequestMapping("/powerball")
 @RequiredArgsConstructor
 public class PowerballController {
     private final PowerballlService service;
+    private final FileProcessing fileProcessing;
+    private final PowerballRepository repo;
 
     @GetMapping("/ping")
     public String greeting() {
@@ -58,6 +64,12 @@ public class PowerballController {
         } catch (Exception e) {
             return errorResponse();
         }
+    }
+
+    @PostMapping("/persist")
+    public ResponseEntity<?> persistFile() {
+        fileProcessing.returnJsonAsList().forEach(repo::saveAndFlush);
+        return ResponseEntity.ok().build();
     }
 
     private ResponseEntity<String> errorResponse() {
