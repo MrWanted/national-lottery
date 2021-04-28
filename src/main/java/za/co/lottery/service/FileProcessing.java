de9febc897fff145d8f148ed38b3d6969d900017
Amos Sekati <amos.sekati@momentum.co.za>
2021-04-28 22:41:44 +0200

    Add persist code

--- src/main/java/za/co/lottery/service/FileProcessing.java
+++ src/main/java/za/co/lottery/service/FileProcessing.java
@@ -8,21 +8,20 @@ import lombok.Data;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import org.springframework.stereotype.Component;
 import za.co.lottery.model.Powerball;
-import za.co.lottery.repository.PowerballRepository;
 
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Data
+@Component
 public class FileProcessing {
 
-    private final PowerballRepository repository;
-
-    private  JSONObject returnJsonAsList() {
+    public List<Powerball> returnJsonAsList() {
 
         JSONParser jsonParser = new JSONParser();
 
@@ -31,41 +30,18 @@ public class FileProcessing {
             List<Object> data = (List<Object>) jsonObject.get("data");
             final Gson gson = new GsonBuilder().create();
             final ObjectMapper objectMapper = new ObjectMapper();
-            data.stream().map(rec -> {
+            return data.stream().map(rec -> {
                 try {
                     return objectMapper.readValue(gson.toJson(rec), Powerball.class);
                 } catch (JsonProcessingException e) {
                     e.printStackTrace();
                     return null;
                 }
-            }).filter(Objects::nonNull).forEach(repository::saveAndFlush);
-            return jsonObject;
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ParseException e) {
+            }).filter(Objects::nonNull).collect(Collectors.toList());
+        } catch (IOException | ParseException e) {
             e.printStackTrace();
         }
-        return new JSONObject();
+        return null;
     }
 
-
-    public static void main(String[] args) {
-//        returnJsonAsList();
-    }
-
-    public Powerball getPowerball(JSONObject jsonObject) {
-        Powerball powerball = new Powerball();
-        powerball.setDrawDate((String) jsonObject.get("drawDate"));
-        powerball.setDrawNumber(Integer.parseInt((String) jsonObject.get("drawNumber")));
-        powerball.setBall1(Integer.parseInt((String) jsonObject.get("ball1")));
-        powerball.setBall2(Integer.parseInt((String) jsonObject.get("ball2")));
-        powerball.setBall3(Integer.parseInt((String) jsonObject.get("ball3")));
-        powerball.setBall4(Integer.parseInt((String) jsonObject.get("ball4")));
-        powerball.setBall5(Integer.parseInt((String) jsonObject.get("ball5")));
-        powerball.setPowerball(Integer.parseInt((String) jsonObject.get("powerball")));
-
-        return powerball;
-    }
 }
